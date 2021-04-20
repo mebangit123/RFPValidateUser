58305f6013a8efe57e3925922909ae014972a5e2
mebangit123 <mebannongrum333@gmail.com>
2021-04-20 14:49:36 +0530

    Added functionality to validate user's First name

--- RegularExpressionPP/src/ValidateUser.java
+++ RegularExpressionPP/src/ValidateUser.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ValidateUser {
 	public static void main(String[] args) {
 		System.out.println("Welcome User Registration Validation Program");
+		
+		Scanner sc = new Scanner(System.in);
+		String regex = "^[A-Z][a-z][a-z]+$";
+		
+		System.out.println("Enter Your name: ");
+		String fname = sc.nextLine();
+		Pattern pat = Pattern.compile(regex);
+		
+		if(pat.matcher(fname).matches())
+			System.out.println("Name is Valid");
+		else
+			System.out.println("Name is Invalid");
 	}
 }
