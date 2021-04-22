b68f71302d9634d329bfbfc6589c0ecf09eda2ef
mebangit123 <mebannongrum333@gmail.com>
2021-04-22 12:18:40 +0530

    Validate password with minimum 8 characters

--- RegularExpressionPP/src/ValidateUser.java
+++ RegularExpressionPP/src/ValidateUser.java
@@ -15,9 +15,13 @@ public class ValidateUser {
 		System.out.println("Enter Your Email: ");
 		String email = sc.nextLine();
 		System.out.println("Enter Your PhoneNo: ");
-		String phoneNo = sc.next();
+		String phoneNo = sc.nextLine();
+		System.out.println("Enter Your Password: ");
+		String password = sc.nextLine();
 		
-		User user = new User(fname, lname,email,phoneNo);
+		//sc.close();
+		
+		User user = new User(fname, lname,email,phoneNo,password);
 		
 		System.out.println(validateUser(user));
 	}
@@ -29,6 +33,7 @@ public class ValidateUser {
 		String lnameRegex = "^[A-Z][a-z][a-z]+$";
 		String emailRegex = "^[0-9a-zA-Z]+([._-][0-9a-zA-Z])*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
 		String phoneRegex = "^[0-9]{2}[\\s][1-9][0-9]{9}$";
+		String passRegex = "^[0-9a-zA-Z]{8,}$";
 		
 		Pattern pat = Pattern.compile(fnameRegex);
 		Matcher m = pat.matcher(user.getFname());
@@ -42,7 +47,10 @@ public class ValidateUser {
 		pat = Pattern.compile(emailRegex);
 		Matcher m2 = pat.matcher(user.getEmail());
 		
-		if(m.matches() | m1.matches() | m2.matches() | m3.matches() == valid ) {
+		pat = Pattern.compile(passRegex);
+		Matcher m4 = pat.matcher(user.getPassword());
+		
+		if(m.matches() & m1.matches() & m2.matches() & m3.matches() & m4.matches() ) {
 		valid = true;	
 		}
 	return valid;
@@ -54,12 +62,14 @@ class User {
 	private String fname;
 	private String lname;
 	private String email;
-	String phoneNo;
+	private String phoneNo;
+	private String password;
 	
-	public User(String fname, String lname, String email, String phoneNo) {
+	public User(String fname, String lname, String email, String phoneNo, String password) {
 		this.fname = fname;
 		this.lname = lname;
 		this.email = email;
+		this.password = password;
 		this.phoneNo = phoneNo;
 	}
 	
@@ -87,5 +97,10 @@ class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
